794bbbfbfbd540f2b0db6f4412050d14de2e308e
ramjak <rama.jakaria@gmail.com>
2019-07-19 18:24:14 +0700

    create a sample store

--- src/modules/users/store.ts
+++ src/modules/users/store.ts
@@ -4,7 +4,7 @@ import ServiceFactory from '@/services/ServiceFactory';
 import ActionTypes from '@/modules/users/actionTypes';
 import IUser from '@/modules/users/IUser';
 
-interface IUsersState {
+export interface IUsersState {
   list: IAPIResponse<IUser[]>;
 }
 
@@ -20,25 +20,25 @@ const module: Module<IUsersState, IRootState> = {
   },
   actions: {
     fetchUsers: async ({ commit }) => {
-      commit(ActionTypes.FETCH_POSTS);
+      commit(ActionTypes.FETCH_USERS);
       try {
         const posts = await req.get('/users');
-        commit(ActionTypes.INIT_POSTS, posts);
+        commit(ActionTypes.INIT_USERS, posts);
       } catch (e) {
-        commit(ActionTypes.FETCH_POSTS_ERROR, e);
+        commit(ActionTypes.FETCH_USERS_ERROR, e);
       }
     },
   },
   mutations: {
-    [ActionTypes.FETCH_POSTS]: (state) => {
+    [ActionTypes.FETCH_USERS]: (state) => {
       state.list.isFetching = true;
       state.list.error = undefined;
     },
-    [ActionTypes.INIT_POSTS]: (state, payload: IUsersState['list']['data']) => {
+    [ActionTypes.INIT_USERS]: (state, payload: IUsersState['list']['data']) => {
       state.list.data = payload;
       state.list.isFetching = false;
     },
-    [ActionTypes.FETCH_POSTS_ERROR]: (state, payload: Error) => {
+    [ActionTypes.FETCH_USERS_ERROR]: (state, payload: Error) => {
       state.list.error = payload;
       state.list.isFetching = false;
     },
