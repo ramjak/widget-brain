97cc6aab5f81a5d4dc4f7b69543302795f9cf2ae
ramjak <rama.jakaria@gmail.com>
2019-07-19 11:52:07 +0700

    add auth service

---
+++ src/services/AuthService.ts
@@ -0,0 +1,36 @@
+import IAuthService from './IAuthService';
+import IPersistentStorage from './IPersistentStorage';
+
+export default class AuthService implements IAuthService {
+  public static readonly AUTH_KEY = '_app_';
+  public static readonly DAY_AFTER_EXPIRED = 7;
+
+  private storage: IPersistentStorage;
+
+  constructor(storage: IPersistentStorage) {
+    this.storage = storage;
+  }
+
+  public deleteToken(): void {
+    this.storage.remove(AuthService.AUTH_KEY);
+  }
+
+  public getToken(): string {
+    const val = this.storage.get(AuthService.AUTH_KEY);
+    if (!val) {
+      throw new Error('Token doesn\'t exist');
+    }
+    return val;
+  }
+
+  public refreshToken(): void {
+    const currentToken = this.getToken();
+    this.saveToken(currentToken);
+  }
+
+  public saveToken(token: string): void {
+    this.storage.set(AuthService.AUTH_KEY, token, {
+      expires: AuthService.DAY_AFTER_EXPIRED,
+    });
+  }
+}
---
+++ src/services/CookieService.ts
@@ -0,0 +1,20 @@
+import {
+  get as getCookie,
+  remove as removeCookie,
+  set as setCookie,
+} from 'es-cookie';
+import IPersistentStorage, { IStorageOptions } from './IPersistentStorage';
+
+const cookieService: IPersistentStorage = {
+  get(name: string): string | undefined {
+    return getCookie(name);
+  },
+  set(name: string, value: string, options?: IStorageOptions): void {
+    setCookie(name, value, options);
+  },
+  remove(name) {
+    removeCookie(name);
+  },
+};
+
+export default cookieService;
