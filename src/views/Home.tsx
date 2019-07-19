77dcc6b65aadf8851967a9ad17934821313093bf
ramjak <rama.jakaria@gmail.com>
2019-07-19 10:22:43 +0700

    add data example to Home

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,11 +3,30 @@ import {VNode} from 'vue';
 import HelloWorld from '@/components/HelloWorld.tsx';
 // @ts-ignore
 import logo from '../assets/logo.png';
+import {InputHTMLAttributes, SyntheticEvent} from 'vue-tsx-support/types/dom';
 
 const Home = tsx.componentFactory.create({
+  data() {
+    return {
+      a: 0,
+    };
+  },
+  methods: {
+    increaseCounter() {
+      this.a = this.a + 1;
+    },
+    changeCounter(e: SyntheticEvent<InputHTMLAttributes, Event>) {
+      const value = e.target.value;
+      const newA = typeof value === 'string' ? value : '0';
+      this.a = parseInt(newA, 10);
+    },
+  },
   render(): VNode {
     return <div class='home'>
-      <img alt='Vue logo' src={logo} />
+      <img alt='Vue logo' src={logo} /> <br />
+      <input type='number' value={this.a} onChange={this.changeCounter} />
+      <br />
+      <button onClick={this.increaseCounter}>Click Me!</button>
       <HelloWorld msg='Welcome to Your Vue.js + TypeScript' />
     </div>;
   },
