47d8c42aa61eec42c16eb11596a6986ec3ed421c
ramjak <rama.jakaria@gmail.com>
2019-07-19 20:42:09 +0700

    add template

---
+++ src/modules/torques/views/Torques.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import Vue from 'vue';
+import Header from '../components/Header.vue';
+import Sidebar from '../components/Sidebar.vue';
+
+const Torques = {
+  components: {
+    Header,
+    Sidebar,
+  },
+  data() {
+    return {
+      nav: [
+        {
+          name: 'Dashboard',
+          url: '/dashboard',
+          icon: 'fa fa-dashboard',
+          badge: {
+            variant: 'success',
+            text: 'NEW',
+          },
+        },
+      ],
+    };
+  },
+};
+
+export default Torques;
+</script>
+
+<template>
+  <div class="admin-container">
+    <Sidebar :navItems="nav"/>
+    <div id="right-panel" class="right-panel">
+      <Header />
+      <div class="content pb-0">
+        <transition enter-active-class="animated fadeIn">
+          <router-view></router-view>
+        </transition>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @import url('../assets/css/font-awesome.min.css');
+  @import url('../assets/css/themify-icons.css');
+  @import url('../assets/css/pe-icon-7-filled.css');
+  @import url('../assets/css/flag-icon.min.css');
+  @import url('../assets/css/cs-skin-elastic.css');
+
+  @import "../assets/scss/style";
+
+  button {
+    cursor: pointer;
+  }
+</style>
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,36 +1,10 @@
 import * as tsx from 'vue-tsx-support';
 import {VNode} from 'vue';
-import HelloWorld from '@/components/HelloWorld.tsx';
-// @ts-ignore
-import logo from '../assets/logo.png';
-import {InputHTMLAttributes, SyntheticEvent} from 'vue-tsx-support/types/dom';
-import Users from '@/modules/users/views/Users.vue';
+import Torques from '@/modules/torques/views/Torques.vue';
 
 const Home = tsx.componentFactory.create({
-  data() {
-    return {
-      a: 0,
-    };
-  },
-  methods: {
-    increaseCounter() {
-      this.a = this.a + 1;
-    },
-    changeCounter(e: SyntheticEvent<InputHTMLAttributes, Event>) {
-      const value = e.target.value;
-      const newA = typeof value === 'string' ? value : '0';
-      this.a = parseInt(newA, 10);
-    },
-  },
   render(): VNode {
-    return <div class='home'>
-      <img alt='Vue logo' src={logo} /> <br />
-      <input type='number' value={this.a} onChange={this.changeCounter} />
-      <br />
-      <button onClick={this.increaseCounter}>Click Me!</button>
-      <Users />
-      <HelloWorld msg='Welcome to Your Vue.js + TypeScript' />
-    </div>;
+    return <Torques />;
   },
 });
 
